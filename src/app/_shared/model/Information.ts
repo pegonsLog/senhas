a1e1f0356ee6f72d40d6138bf58f5b10394d16aa
pegonsLog <pegons.log@gmail.com>
2023-03-23 17:04:03 -0300

    first commit

---
+++ src/app/_shared/model/Information.ts
@@ -0,0 +1,7 @@
+export interface Information {
+  id: number,
+  name: string,
+  service: string,
+  corporation: string,
+  password: string
+}
---
+++ src/app/components/informations/informations.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InformationsComponent } from './informations.component';
+
+
+
+@NgModule({
+  declarations: [
+    InformationsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InformationsModule { }
